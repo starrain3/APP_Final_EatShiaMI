887e2addd5a4e816e5af4b0552e3b247c5a4b136
Ku <jasonku3@gmail.com>
2023-05-25 16:45:57 +0800

    home page modified v1

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, 
-        Image, Button, Dimensions } from 'react-native';
+          Image, Button, Dimensions,
+          TouchableOpacity } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style = {styles.mainTitle}>吃鯊米?</Text>
-      <Image style = {styles.mainImg} source={require('./assets/whatToEat.png')} ></Image>
-      <Button title="吃飯去"/>
+      <HomeTitle></HomeTitle>
+      <HomeImg></HomeImg>
+      <HomeButton></HomeButton>
+      
 
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const HomeTitle = () => {
+  return (
+    <Text style = {styles.homeTitle}>吃鯊米?</Text>
+  );
+};
+
+const HomeImg = () => {
+  return (
+    <Image style = {styles.homeImg} source={require('./assets/whatToEat.png')} ></Image>
+  );
+};
+
+const HomeButton = () => {
+  return (
+    <TouchableOpacity style={styles.homeButton} onPress={() => console.log('Button clicked')}>
+      <Text style={styles.homeButtonText}>吃飯去</Text>
+    </TouchableOpacity>
+    );
+}
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -24,14 +49,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  mainImg : {
+  homeImg : {
     // marginBottom: "10rem",
-    height: windowWidth,
+    height: windowHeight/2,
     width: windowWidth,
     resizeMode: "stretch"
   },
 
-  mainTitle : {
+  homeTitle : {
     fontSize: 100
+  },
+
+  homeButton: {
+
+    width: windowWidth,
+    backgroundColor: '#000',
+    height: windowHeight/4,
+    // paddingTop: 50 ,
+    alignItems: "center",
+    justifyContent: 'center'
+
+  },
+
+  homeButtonText: {
+    color: "#fff"
   }
+
 });
